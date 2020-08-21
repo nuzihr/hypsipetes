fb6f2972ad0a4bcd9c93b27f505037a7261713c5
nuzihr <naoya.ujihara@gmail.com>
2020-08-21 14:49:21 +0900

    add weapons module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { SeasonsModule } from './seasons/seasons.module';
 import { OperatorsModule } from './operators/operators.module';
+import { WeaponsModule } from './weapons/weapons.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as https from 'https';
@@ -9,6 +10,7 @@ import * as https from 'https';
   imports: [
     SeasonsModule,
     OperatorsModule,
+    WeaponsModule,
     HttpModule.register({
       httpsAgent: new https.Agent({ keepAlive: true }),
     }),
---
+++ src/weapons/weapons.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { members } from '../app.service';
+import { StatsRepository } from '../stats.repository';
+import { Stats } from '../stats.entity';
+
+@Injectable()
+export class WeaponsService {
+  statsRepository;
+  constructor() {
+    this.statsRepository = StatsRepository.getInstance();
+  }
+
+  async getWeapon(weaponName: string) {
+    const membersOperator = members.map(memberName => {
+      const stats: Stats = this.statsRepository.findByName(memberName);
+      const kd = stats.getKdByWeapon(weaponName);
+      const headshot = stats.getHeadShotPercentageByWeapon(weaponName);
+      const playedTimes = stats.getPlayedTimesByWeapon(weaponName);
+      return { [memberName]: { x: kd, y: headshot, z: playedTimes } };
+    });
+    return Object.assign({}, ...membersOperator);
+  }
+}
